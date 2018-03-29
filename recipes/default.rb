8975b603992bed57e3c4b73833646b275cdef677
wangjhg <wangjhg@yahoo.com>
2018-03-29 17:47:13 -0400

    first commit

--- recipes/default.rb
+++ recipes/default.rb
@@ -3,3 +3,12 @@
 # Recipe:: default
 #
 # Copyright:: 2018, The Authors, All Rights Reserved.
+
+node.default['haproxy']['members'] = [{
+    "hostname" => "ec2-34-228-238-125.compute-1.amazonaws.com",
+    "ipaddress" => "34.228.238.125",
+    "port" => 80,
+    "ssl_port" => 80
+  }]
+
+include_recipe 'haproxy::manual'
