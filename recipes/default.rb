a8e97ceb525dc244dd5a02d25e2268b7402552d3
wangjhg <wangjhg@yahoo.com>
2018-03-30 12:53:41 -0400

    version 1.0.0, dynamic recipe for lb

--- recipes/default.rb
+++ recipes/default.rb
@@ -4,17 +4,22 @@
 #
 # Copyright:: 2018, The Authors, All Rights Reserved.
 
-node.default['haproxy']['members'] = [{
-    "hostname" => "ec2-34-228-238-125.compute-1.amazonaws.com",
-    "ipaddress" => "34.228.238.125",
-    "port" => 80,
-    "ssl_port" => 80
-  },
-{
-  "hostname" => "ec2-54-164-189-222.compute-1.amazonaws.com",
-  "ipaddress" => "54.164.189.222",
-  "port" => 80,
-  "ssl_port" => 80
-  }]
+all_web_nodes = search('node','role:web_server')
+
+members = []
+
+all_web_nodes.each do |web_node|
+  member = {
+    'hostname' => web_node['cloud']['public_hostname'],
+    'ipaddress' => web_node['cloud']['public_ipv4'],
+    'port' => 80,
+    'ssl_port' => 80
+  }
+
+  members.push(member)
+
+end
+
+node.default['haproxy']['members'] = members
 
 include_recipe 'haproxy::manual'
